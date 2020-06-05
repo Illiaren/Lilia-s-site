50fcc724da7efbd000040ff6c9e4f5e2e6940a11
Toma <flammenden.regen@gmail.com>
2020-06-05 21:53:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use script'
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+
+// alert("Hello World!");
+
+let answer = prompt("Есть ли Вам 18?, Да");
+
+console.log(typeof (arr));
